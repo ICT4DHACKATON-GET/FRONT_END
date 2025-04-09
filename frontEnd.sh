3929d6d69b19e10b52d8b0d9637fa407b783a114
Lembou Pharel <lemboupharel@proton.me>
2025-04-09 19:53:24 +0100

    maquet des pages

---
+++ frontEnd.sh
@@ -0,0 +1,7 @@
+echo "# FRONT_END" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:ICT4DHACKATON-GET/FRONT_END.git
+git push -u origin main

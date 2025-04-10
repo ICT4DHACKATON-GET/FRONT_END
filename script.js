7b9856ac606c2b2e67c47b5c1f2984b5ff5f670f
esthera-tiago <estheratiago@gmail.com>
2025-04-10 01:47:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const ip = "http://192.168.1.142:5000/";
-
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
@@ -11,67 +9,3 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
-
-
-
-// connexion back-end
-
-const register = document.getElementById('sigin');
-const logi = document.getElementById('login1');
-
-register.addEventListener('click', () => {
-    alert("sigin");
-    registrer();
-});
-
-logi.addEventListener('click', () => {
-    alert("login");
-    login();
-});
-
-function login(){
-    fetch(ip+"login", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    email: "tsanga@gmail.com",
-    num: "620142649",
-    password: "fdfsdfd"
-  })
-})
-.then(response => response.json())
-.then(data => {
-  console.log("Réponse du serveur :", data);
-})
-.catch(error => {
-  console.error("Erreur :", error);
-});
-
-}
-
-function registrer(){
-    fetch(ip+"sigin", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    name:"tsanga awana ddfd",
-    password:"dfdfdfdf",
-    operateur:"MTN",
-    email: "tsanga@gmail.com",
-    num: "620142649",
-    password: "fdfsdfd"
-  })
-})
-.then(response => response.json())
-.then(data => {
-  console.log("Réponse du serveur :", data);
-})
-.catch(error => {
-  console.error("Erreur :", error);
-});
-
-}

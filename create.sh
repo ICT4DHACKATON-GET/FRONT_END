4c5cdf3e7a48adaa75e9316ec2dcfbdce9b4f02d
Lembou Pharel <lemboupharel@proton.me>
2025-04-10 03:05:20 +0100

    commite 0

---
+++ create.sh
@@ -0,0 +1,7 @@
+echo "# Front_end" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:ICT4DHACKATON-GET/Front_end.git
+git push -u origin main
